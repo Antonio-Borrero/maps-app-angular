ece0041869524a5eb8a75ee8596f19f25f725524
Antonio <antojoseabad@gmail.com>
2026-04-03 12:46:48 +0200

    feat: add dynamic  zoom control on map

--- src/app/pages/fullscreen-map-page/fullscreen-map-page.ts
+++ src/app/pages/fullscreen-map-page/fullscreen-map-page.ts
@@ -1,36 +1,69 @@
-import { AfterViewInit, Component, ElementRef, viewChild } from '@angular/core';
+import { AfterViewInit, Component, effect, ElementRef, signal, viewChild } from '@angular/core';
 import mapboxgl from 'mapbox-gl';
 import { environment } from '../../../environments/environment';
+import { DecimalPipe } from '@angular/common';
 
 mapboxgl.accessToken = environment.mapboxKey;
 
 @Component({
   selector: 'app-fullscreen-map-page',
-  imports: [],
+  imports: [DecimalPipe],
   templateUrl: './fullscreen-map-page.html',
   styles: `
     div {
       width: 100vw;
       height: calc(100vh - 64px);
     }
+
+    #controls {
+      background-color: white;
+      padding: 10px;
+      border-radius: 5px;
+      position: fixed;
+      bottom: 25px;
+      right: 20px;
+      z-index: 9999;
+      box-shadow: 0 0 10px 0 rgba($color: 0, $alpha: 1);
+      border: 1px solid #e2e8f0;
+      width: 250px;
+    }
   `,
 })
 export class FullscreenMapPage implements AfterViewInit {
   divElement = viewChild<ElementRef>('map');
 
-  async ngAfterViewInit() {
+  map = signal<mapboxgl.Map | null>(null);
+  zoom = signal(14);
 
+  zoomEffect = effect(() => {
+    if (!this.map()) return;
+
+    this.map()?.setZoom(this.zoom());
+  });
+
+  async ngAfterViewInit() {
     if (!this.divElement()?.nativeElement) return;
 
-    await new Promise((resolve) => setTimeout(resolve, 80))
+    await new Promise((resolve) => setTimeout(resolve, 80));
 
     const element = this.divElement()!.nativeElement;
-    console.log(element)
 
-    const map = new mapboxgl.Map({
+    const mapbox = new mapboxgl.Map({
       container: element, // container ID
       center: [-71.06776, 42.35816], // starting position [lng, lat]. Note that lat must be set between -90 and 90
-      zoom: 9, // starting zoom
+      zoom: this.zoom(), // starting zoom
     });
+
+    this.mapListeners(mapbox)
+  }
+  
+  mapListeners( map: mapboxgl.Map ){
+    
+    map.on('zoomend', (event) => {
+      const newZoom = event.target.getZoom();
+      this.zoom.set(newZoom)
+    }) 
+
+    this.map.set(map);
   }
 }
