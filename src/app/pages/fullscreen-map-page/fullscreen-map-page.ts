081cf313f9ea909a9dbdd3d3c136cb8e38f913e2
Antonio <antojoseabad@gmail.com>
2026-04-03 11:43:01 +0200

    feat: integrate API and render map

--- src/app/pages/fullscreen-map-page/fullscreen-map-page.ts
+++ src/app/pages/fullscreen-map-page/fullscreen-map-page.ts
@@ -1,8 +1,36 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, viewChild } from '@angular/core';
+import mapboxgl from 'mapbox-gl';
+import { environment } from '../../../environments/environment';
+
+mapboxgl.accessToken = environment.mapboxKey;
 
 @Component({
   selector: 'app-fullscreen-map-page',
   imports: [],
   templateUrl: './fullscreen-map-page.html',
+  styles: `
+    div {
+      width: 100vw;
+      height: calc(100vh - 64px);
+    }
+  `,
 })
-export class FullscreenMapPage { }
+export class FullscreenMapPage implements AfterViewInit {
+  divElement = viewChild<ElementRef>('map');
+
+  async ngAfterViewInit() {
+
+    if (!this.divElement()?.nativeElement) return;
+
+    await new Promise((resolve) => setTimeout(resolve, 80))
+
+    const element = this.divElement()!.nativeElement;
+    console.log(element)
+
+    const map = new mapboxgl.Map({
+      container: element, // container ID
+      center: [-71.06776, 42.35816], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+      zoom: 9, // starting zoom
+    });
+  }
+}
